c437335df9c5aa559c0e7d36eb75823b21612024
Emmanuel <nwodoemmanuel@gmail.com>
2025-03-18 20:22:55 +0100

    created the frequently asked question and toggled it with javascript

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ window.addEventListener('scroll', () =>{
   nav.classList.remove('window-scroll')
 }
 });
+
+
+//open the faq answers onclick//
+
+const faqs =document.querySelectorAll('.faq');
+
+faqs.forEach(faq =>{
+faq.addEventListener('click', () => {
+  faq.classList.toggle('open');
+
+  // change icon//
+  const icon =faq.querySelector('.faq_icon i')
+  if(icon.className=== "fa-solid fa-plus"){
+    icon.className = "fa-solid fa-minus"
+  } else {
+    icon.className ="fa-solid fa-plus"
+  }
+});
+});
